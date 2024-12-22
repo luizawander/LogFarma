469edc2034dba1ccd7a0ec106e1180cbaa1e42cd
Luiza Sales <154068580+luizawander@users.noreply.github.com>
2024-12-21 21:03:18 -0300

    criando tabela medicamento e inicializando sql_app.db

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from app.database import engine, Base
+from app.models import medicamento
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="LogFarma API",
